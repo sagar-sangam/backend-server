c064200df7f8535ee9d09141957f8ebb37340dc0
Sagar Kumar <sagarverma.verm@gmail.com>
2026-05-29 20:24:59 +0530

    commit

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  url: "mongodb://127.0.0.1:27017/jsr",
+  // url: "mongodb://127.0.0.1:27017/jsr",
+  url:"mongodb+srv://sagar:Sagar@12@cluster0.2qqb4bf.mongodb.net/?appName=Cluster0"
 };
